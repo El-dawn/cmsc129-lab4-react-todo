e347bf981d3181b4ce7c16dda8080b2933f3daa2
Don <esfpabua@gmail.com>
2025-07-23 09:53:20 +0800

    Connected app to firebase

--- configuration.jsx
+++ configuration.jsx
@@ -1,25 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyBMarCDvL22EuJMxNoUdUuF-Xnozs1JP1w",
-	authDomain: "todo-app-react-4de6c.firebaseapp.com",
-	databaseURL: "https://todo-app-react-4de6c-default-rtdb.firebaseio.com",
-	projectId: "todo-app-react-4de6c",
-	storageBucket: "todo-app-react-4de6c.firebasestorage.app",
-	messagingSenderId: "24640178812",
-	appId: "1:24640178812:web:d472d9b4fd637b3d5b17f9",
-	measurementId: "G-B7BWMRNCSD",
+	apiKey: "AIzaSyC1Mr8NIj5Sm8ClLDtZ8-YKFXmbglXjntE",
+	authDomain: "todo-list-d59cb.firebaseapp.com",
+	projectId: "todo-list-d59cb",
+	storageBucket: "todo-list-d59cb.firebasestorage.app",
+	messagingSenderId: "942801691578",
+	appId: "1:942801691578:web:7793693f7adb558a817383",
+	measurementId: "G-89YVX9LBEL",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 
 export default app;
